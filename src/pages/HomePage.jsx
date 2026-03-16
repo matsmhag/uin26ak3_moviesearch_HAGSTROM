5446246e2c99212d30c33c436ce8210caf2238ae
matsmhag <hagstr20@gmail.com>
2026-03-16 13:21:07 +0100

    la til homepage og moviepage component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <section>
+      <h2>Forside</h2>
+      <p>Her skal filmsøket komme.</p>
+    </section>
+  );
+}
---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,8 @@
+export default function MoviePage() {
+  return (
+    <section>
+      <h2>Film</h2>
+      <p>Her kommer informasjon om valgt film.</p>
+    </section>
+  );
+}
