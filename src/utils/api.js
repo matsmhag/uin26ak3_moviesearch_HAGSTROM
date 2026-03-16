d21a99a66827a3be5cf0e3268264d770dd571d8d
matsmhag <hagstr20@gmail.com>
2026-03-16 12:47:30 +0100

    La til api funksjoner

---
+++ src/utils/api.js
@@ -0,0 +1,20 @@
+const API_KEY = "b8428fde";
+const BASE_URL = "https://www.omdbapi.com/";
+
+export async function fetchMovies(searchTerm) {
+  const response = await fetch(
+    `${BASE_URL}?apikey=${API_KEY}&s=${searchTerm}&type=movie`
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+export async function fetchMovieById(id) {
+  const response = await fetch(
+    `${BASE_URL}?apikey=${API_KEY}&i=${id}&plot=full`
+  );
+
+  const data = await response.json();
+  return data;
+}
